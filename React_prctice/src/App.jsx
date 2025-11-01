9e300a697306b719c135571d92c901789cd750e3
nerfnyanit <nerfnyanit@gmail.com>
2025-11-01 20:17:47 +0900

    Fix Header import and update App.jsx

--- React_prctice/src/App.jsx
+++ React_prctice/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import './App.css'
-import Header from './components/header'
+import Header from './components/Header'
 import MyButton from './components/MyButton'
 
 function App() {
